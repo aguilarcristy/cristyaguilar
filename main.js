f4f9abaf5a9e054dc0840bb950ed8f3bcba10bec
aguilarcristy <cristyaguilar3101@gmail.com>
2024-04-17 23:18:10 -0700

    Footer, Image Folders & Social Icons

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('mousemove', function(e) {
+    var customCursor = document.querySelector('.custom-cursor');
+    customCursor.style.left = e.clientX + 'px';
+    customCursor.style.top = e.clientY + 'px';
+});
